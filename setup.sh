15f3a87ac79324ce94228ac0ab3866e96a595571
Samer Masterson <nosefrog@gmail.com>
2013-10-16 06:58:16 -0400

    remove diff stuff

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ for i in $(ls -a) ; do
         if [ $i = $j ] ; then
             CAN_COPY=0
             break # efficiency is key!
->>>>>>> 39c96f9c894211ced39b26dd0d812ea2727d19d1
         fi
     done
     if [ $CAN_COPY -ne 0 ] ; then
