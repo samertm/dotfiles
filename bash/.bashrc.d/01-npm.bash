e5e15d0564bc0a6697859271ee56a9bd6e90c416
Samer Masterson <samer@samertm.com>
2016-02-03 17:17:15 -0800

    update npm/nvm stuff

--- bash/.bashrc.d/01-npm.bash
+++ bash/.bashrc.d/01-npm.bash
@@ -1,12 +1,13 @@
-if has npm; then
-    if [[ ! -e $HOME/.npm-packages ]]; then
-        mkdir $HOME/.npm-packages
-    fi
-    export NPM_PACKAGES="${HOME}/.npm-packages"
-    export NODE_PATH="$NPM_PACKAGES/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
-    export PATH="$NPM_PACKAGES/bin:$PATH"
+# Disable for now b/c nvm doesn't support 'PREFIX'.
+# if has npm; then
+#     if [[ ! -e $HOME/.npm-packages ]]; then
+#         mkdir $HOME/.npm-packages
+#     fi
+#     export NPM_PACKAGES="${HOME}/.npm-packages"
+#     export NODE_PATH="$NPM_PACKAGES/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+#     export PATH="$NPM_PACKAGES/bin:$PATH"
 
-    # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
-    unset MANPATH # delete if you already modified MANPATH elsewhere in your config
-    export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-fi
+#     # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+#     unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+#     export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+# fi
--- bash/.bashrc.d/nvm.bash
+++ bash/.bashrc.d/nvm.bash
@@ -1,4 +1,4 @@
-if [ -f $HOME/.nvm ]; then
+if [ -d $HOME/.nvm ]; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 fi
